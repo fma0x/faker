df05fa65e81a22d6345440e18df1efdb448ca892
sjerusallem <paulobojji@gmail.com>
2023-08-18 23:59:25 -0300

    Feat: RandomCity return string with country names

--- anime/fma/fmabrotherhood.go
+++ anime/fma/fmabrotherhood.go
@@ -17,3 +17,9 @@ func RandomCity() string {
 	city := en.Cities[rand.Intn(len(en.Cities))]
 	return city
 }
+
+// Random FullMetal Alchemist Brotherhood countries names.
+func RandomCountry() string {
+	country := en.Countries[rand.Intn(len(en.Countries))]
+	return country
+}
