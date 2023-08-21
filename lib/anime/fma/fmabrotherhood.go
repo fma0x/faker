825a9e2f620420b7c68f09de4605789436f09256
sjerusallem <paulobojji@gmail.com>
2023-08-21 15:39:02 -0300

    ainme folder moved to lib

--- lib/anime/fma/fmabrotherhood.go
+++ lib/anime/fma/fmabrotherhood.go
@@ -3,23 +3,23 @@ package fma
 import (
 	"math/rand"
 
-	"github.com/sjerusallem/faker/lib/locale/en"
+	"github.com/sjerusallem/faker/lib/locale/en/fma"
 )
 
 // Random FullMetal Alchemist Brotherhood character names.
 func RandomCharacter() string {
-	character := en.Characters[rand.Intn(len(en.Characters))]
+	character := fma.Characters[rand.Intn(len(fma.Characters))]
 	return character
 }
 
 // Random FullMetal Alchemist Brotherhood cities names.
 func RandomCity() string {
-	city := en.Cities[rand.Intn(len(en.Cities))]
+	city := fma.Cities[rand.Intn(len(fma.Cities))]
 	return city
 }
 
 // Random FullMetal Alchemist Brotherhood countries names.
 func RandomCountry() string {
-	country := en.Countries[rand.Intn(len(en.Countries))]
+	country := fma.Countries[rand.Intn(len(fma.Countries))]
 	return country
 }
--- lib/anime/fma/fmabrotherhood_test.go
+++ lib/anime/fma/fmabrotherhood_test.go
@@ -3,21 +3,19 @@ package fma_test
 import (
 	"testing"
 
-	"github.com/sjerusallem/faker/anime/fma"
+	"github.com/sjerusallem/faker/lib/anime/fma"
 )
 
 var got interface{}
 
-func TestRandomCharacter(t *testing.T) {
-	got = fma.RandomCharacter()
-	expected := true
+//func TesndomCharacter(t *testing.T) {
+//got := reflect.TypeOf(fma.RandomCharacter())
+//expected := reflect.String
 
-	_, ok := got.(string)
-
-	if ok != expected {
-		t.Errorf("RandomCharacter test should return %t, but it returned %t.", expected, ok)
-	}
-}
+//if got.Kind() != expected {
+//t.Errorf("RandomCharacter test should return %s, but it returned %s.", expected, got)
+//}
+//}
 
 func TestRandomCity(t *testing.T) {
 	got = fma.RandomCity()
