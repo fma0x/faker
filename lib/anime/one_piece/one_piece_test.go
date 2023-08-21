a8a57de73aefd0a5c572cbd8a06dd276037bfd65
sjerusallem <paulobojji@gmail.com>
2023-08-21 16:58:22 -0300

    Test: One Piece Random Data

---
+++ lib/anime/one_piece/one_piece_test.go
@@ -0,0 +1,64 @@
+package one_piece_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sjerusallem/faker/lib/anime/one_piece"
+)
+
+func TestOnePieceRandomData(t *testing.T) {
+	t.Run("GenerateRandomCharacters", func(t *testing.T) {
+		got := reflect.TypeOf(one_piece.RandomCharacter())
+		expected := reflect.String
+
+		if got.Kind() != expected {
+			t.Errorf("RandomCharacter test should return %v, but it returned %v.", expected, got)
+		}
+	})
+
+	t.Run("GenerateRandomAkumaNoMi", func(t *testing.T) {
+		got := reflect.TypeOf(one_piece.RandomAkumaNoMi())
+		expected := reflect.String
+
+		if got.Kind() != expected {
+			t.Errorf("RandomAkumaNoMi test should return %v, but it returned %v.", expected, got)
+		}
+	})
+
+	t.Run("GenerateRandomIsland", func(t *testing.T) {
+		got := reflect.TypeOf(one_piece.RandomIsland())
+		expected := reflect.String
+
+		if got.Kind() != expected {
+			t.Errorf("RandomIsland test should return %v, but it returned %v.", expected, got)
+		}
+	})
+
+	t.Run("GenerateRandomLocation", func(t *testing.T) {
+		got := reflect.TypeOf(one_piece.RandomLocation())
+		expected := reflect.String
+
+		if got.Kind() != expected {
+			t.Errorf("RandomLocation test should return %v, but it returned %v.", expected, got)
+		}
+	})
+
+	t.Run("GenerateRandomQoute", func(t *testing.T) {
+		got := reflect.TypeOf(one_piece.RandomQuote())
+		expected := reflect.String
+
+		if got.Kind() != expected {
+			t.Errorf("RandomQuote test should return %v, but it returned %v.", expected, got)
+		}
+	})
+
+	t.Run("GenerateRandomSea", func(t *testing.T) {
+		got := reflect.TypeOf(one_piece.RandomSea())
+		expected := reflect.String
+
+		if got.Kind() != expected {
+			t.Errorf("RandomSea test should return %v, but it returned %v.", expected, got)
+		}
+	})
+}
