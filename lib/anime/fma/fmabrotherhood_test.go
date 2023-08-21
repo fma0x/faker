2ccafcfd2eff175bff5abf3d25cbdf0fbbac74c1
sjerusallem <paulobojji@gmail.com>
2023-08-21 16:19:06 -0300

    Test: improved tests with reflect

--- lib/anime/fma/fmabrotherhood_test.go
+++ lib/anime/fma/fmabrotherhood_test.go
@@ -1,40 +1,37 @@
 package fma_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/sjerusallem/faker/lib/anime/fma"
 )
 
-var got interface{}
+func TestFMARandomData(t *testing.T) {
+	t.Run("GenerateRandomCharacters", func(t *testing.T) {
+		got := reflect.TypeOf(fma.RandomCharacter())
+		expected := reflect.String
 
-//func TesndomCharacter(t *testing.T) {
-//got := reflect.TypeOf(fma.RandomCharacter())
-//expected := reflect.String
+		if got.Kind() != expected {
+			t.Errorf("RandomCharacter test should return %v, but it returned %v.", expected, got)
+		}
+	})
 
-//if got.Kind() != expected {
-//t.Errorf("RandomCharacter test should return %s, but it returned %s.", expected, got)
-//}
-//}
+	t.Run("GenerateRandomCity", func(t *testing.T) {
+		got := reflect.TypeOf(fma.RandomCity())
+		expected := reflect.String
 
-func TestRandomCity(t *testing.T) {
-	got = fma.RandomCity()
-	expected := true
+		if got.Kind() != expected {
+			t.Errorf("RandomCity test should return %v, but it returned %v.", expected, got)
+		}
+	})
 
-	_, ok := got.(string)
+	t.Run("GenerateRandomCountry", func(t *testing.T) {
+		got := reflect.TypeOf(fma.RandomCountry())
+		expected := reflect.String
 
-	if ok != expected {
-		t.Errorf("RandomCity test should return %t, but it returned %t.", expected, ok)
-	}
-}
-
-func TestRandomCountry(t *testing.T) {
-	got = fma.RandomCountry()
-	expected := true
-
-	_, ok := got.(string)
-
-	if ok != expected {
-		t.Errorf("RandomCountry test should return %t, but it returned %t.", expected, ok)
-	}
+		if got.Kind() != expected {
+			t.Errorf("RandomCountry test should return %v, but it returned %v.", expected, got)
+		}
+	})
 }
