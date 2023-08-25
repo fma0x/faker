7357976f1af1639084518d6c8ba4dd501015d52e
sjerusallem <paulobojji@gmail.com>
2023-08-25 12:21:55 -0300

    Test: PT-BR random name

---
+++ lib/ptbr/firt_name_test.go
@@ -0,0 +1,19 @@
+package ptbr_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sjerusallem/faker/lib/ptbr"
+)
+
+func TestPtBrRandomName(t *testing.T) {
+	t.Run("GenerateRandomName", func(t *testing.T) {
+		got := reflect.TypeOf(ptbr.RandomFirstName())
+		expected := reflect.String
+
+		if got.Kind() != expected {
+			t.Errorf("RandomName test should return %v, but it returned %v.", expected, got)
+		}
+	})
+}
